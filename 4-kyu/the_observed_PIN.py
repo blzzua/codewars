1c62c2b5fb0ab4b2437bb44ee0030c398ca33a54
blzzua <blzzua@github.com>
2022-07-10 11:40:07 +0300

    https://www.codewars.com/kata/5263c6999e0f40dee200059d/

---
+++ 4-kyu/the_observed_PIN.py
@@ -0,0 +1,16 @@
+# https://www.codewars.com/kata/5263c6999e0f40dee200059d/
+
+import itertools
+def get_pins(observed):
+    pos = {
+        '0': ['0', '8'], 
+        '1': ['1', '2', '4'], 
+        '2': ['1', '2', '3', '5'], 
+        '3': ['2', '3', '6'], 
+        '4': ['1', '4', '5', '7'],
+        '5': ['2', '4', '5', '6', '8'],
+        '6': ['3', '5', '6', '9'],
+        '7': ['4', '7', '8'],
+        '8': ['0', '5', '7', '8', '9'],
+        '9': ['6', '8', '9']}
+    return [ ''.join(comb) for comb in itertools.product(*list(map(pos.get,(observed)))) ]
