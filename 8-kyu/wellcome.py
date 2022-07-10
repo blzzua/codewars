c8c01c30485e3b7489908946f31b0fa7004deafe
blzzua <blzzua@github.com>
2022-07-10 11:15:53 +0300

    https://www.codewars.com/kata/577ff15ad648a14b780000e7

---
+++ 8-kyu/wellcome.py
@@ -0,0 +1,20 @@
+#https://www.codewars.com/kata/577ff15ad648a14b780000e7
+def greet(language):
+    db = {'english': 'Welcome',
+    'czech': 'Vitejte',
+    'danish': 'Velkomst',
+    'dutch': 'Welkom',
+    'estonian': 'Tere tulemast',
+    'finnish': 'Tervetuloa',
+    'flemish': 'Welgekomen',
+    'french': 'Bienvenue',
+    'german': 'Willkommen',
+    'irish': 'Failte',
+    'italian': 'Benvenuto',
+    'latvian': 'Gaidits',
+    'lithuanian': 'Laukiamas',
+    'polish': 'Witamy',
+    'spanish': 'Bienvenido',
+    'swedish': 'Valkommen',
+    'welsh': 'Croeso'}
+    return db[language] if language in db.keys() else  'Welcome'
