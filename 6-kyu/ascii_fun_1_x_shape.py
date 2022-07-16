7fd54005867bad3d6bf2cdc7588ed8f55ab7b0e2
blzzua <blzzua@users.noreply.github.com>
2022-07-17 01:40:57 +0300

    https://www.codewars.com/kata/5906436806d25f846400009b/train/python

---
+++ 6-kyu/ascii_fun_1_x_shape.py
@@ -0,0 +1,9 @@
+# https://www.codewars.com/kata/5906436806d25f846400009b/train/python
+
+import numpy as np
+
+def x(n):
+    a = np.eye(n, dtype=int) | np.fliplr(np.eye(n, dtype=int))
+    return '\n'.join([''.join([{0:'□',1:'■'}[c] for c in row]) for row in a.tolist()])
+  
+  
