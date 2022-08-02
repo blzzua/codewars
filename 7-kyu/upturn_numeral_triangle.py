5f6822679680200d68c26316d0bbb1375d27ccb3
blzzua <blzzua@users.noreply.github.com>
2022-08-02 14:48:30 +0300

    Update upturn_numeral_triangle.py

--- 7-kyu/upturn_numeral_triangle.py
+++ 7-kyu/upturn_numeral_triangle.py
@@ -2,3 +2,13 @@
 
 def pattern(n):
     return '\n'.join(' '*i + (' ' + str((i+1)%10))*(n-i) for i in range(n))
+
+## best practices
+def pattern(n):
+    triangle = ""
+    for i in range(n):
+        triangle += " "*(i + 1)
+        triangle += " ".join(str((i + 1)%10)*(n - i))
+        if i < n - 1:
+            triangle += "\n"
+    return triangle
