678e61feaa388b155cfd14cc1e990525ad840e49
blzzua <blzzua@github.com>
2022-10-07 22:17:27 +0300

    https://www.codewars.com/kata/54557d61126a00423b000a45

---
+++ 7-kyu/shorter_concat_reverse_longer.py
@@ -0,0 +1,8 @@
+# https://www.codewars.com/kata/54557d61126a00423b000a45
+
+def shorter_reverse_longer(a,b):
+    if len(a)>=len(b):
+        return b+a[::-1]+b 
+    else:
+        return a+b[::-1]+a 
+
