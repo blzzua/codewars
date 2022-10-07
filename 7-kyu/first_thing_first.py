06405c30777842ec697d9907733a338d2aaa5254
blzzua <blzzua@github.com>
2022-10-07 22:17:17 +0300

    https://www.codewars.com/kata/5b64334ab1788374fb0000c8

---
+++ 7-kyu/first_thing_first.py
@@ -0,0 +1,9 @@
+# https://www.codewars.com/kata/5b64334ab1788374fb0000c8
+
+def solution(first,last):
+    ''' Find a way to make this work without declaring any variables'''
+    try:
+        return first()
+    finally:
+        last()
+
