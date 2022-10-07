c0268930d4359d9ee389b2452f7f79eee40525d8
blzzua <blzzua@github.com>
2022-10-07 22:17:10 +0300

    https://www.codewars.com/kata/53cf459503f9bbb774000003

---
+++ 8-kyu/name_your_python.py
@@ -0,0 +1,9 @@
+# https://www.codewars.com/kata/53cf459503f9bbb774000003
+
+# Create a Python class that takes a name. 
+# example usage: Python('Bubba')
+class Python:
+    def __init__(self,name="Bubba"):
+        self.name = name
+
+
