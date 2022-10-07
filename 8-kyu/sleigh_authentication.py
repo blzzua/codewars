b25def54794a10d8933303916b769c3c96e54bb6
blzzua <blzzua@github.com>
2022-10-07 22:17:09 +0300

    https://www.codewars.com/kata/52adc142b2651f25a8000643

---
+++ 8-kyu/sleigh_authentication.py
@@ -0,0 +1,9 @@
+# https://www.codewars.com/kata/52adc142b2651f25a8000643
+
+class Sleigh(object):
+    def authenticate(self, name, password):
+        if name == 'Santa Claus'and password == 'Ho Ho Ho!':
+            return True
+        else:
+            return False
+
