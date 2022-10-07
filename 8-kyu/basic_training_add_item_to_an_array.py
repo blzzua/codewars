651779d30f9bc471aec2240e2aee7e412f8f7204
blzzua <blzzua@github.com>
2022-10-07 22:17:10 +0300

    https://www.codewars.com/kata/511f0fe64ae8683297000001

---
+++ 8-kyu/basic_training_add_item_to_an_array.py
@@ -0,0 +1,7 @@
+# https://www.codewars.com/kata/511f0fe64ae8683297000001
+
+# add the value "codewars" to the already defined websites array
+websites_internal = ["codewars"]
+websites.append(websites_internal.pop())
+
+
