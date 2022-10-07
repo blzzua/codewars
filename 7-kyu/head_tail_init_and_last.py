dbb2de2fdd5370fb12ccfbeda946c74de40ec54a
blzzua <blzzua@github.com>
2022-10-07 22:17:12 +0300

    https://www.codewars.com/kata/54592a5052756d5c5d0009c3

---
+++ 7-kyu/head_tail_init_and_last.py
@@ -0,0 +1,17 @@
+# https://www.codewars.com/kata/54592a5052756d5c5d0009c3
+
+# TODO: implement the four functions specified.
+def head(l):
+    return l[0]
+
+def tail(l):
+    return l[1:]
+
+def init(l):
+    return l[:-1]
+
+def last(l):
+    return l[-1]
+    
+
+
