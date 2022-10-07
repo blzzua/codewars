0c1284667ca6f8e91b8b712c78df16d6827b3e9e
blzzua <blzzua@github.com>
2022-10-07 22:17:10 +0300

    https://www.codewars.com/kata/54dba07f03e88a4cec000caf

---
+++ 8-kyu/barking_mad.py
@@ -0,0 +1,14 @@
+# https://www.codewars.com/kata/54dba07f03e88a4cec000caf
+
+class Dog ():
+    def __init__(self, breed):
+        self.breed = breed
+        
+    @staticmethod       
+    def bark():
+        return "Woof"
+    
+
+snoopy = Dog("Beagle")
+scoobydoo = Dog("Great Dane")
+
