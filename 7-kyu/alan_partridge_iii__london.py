ad63f54962110ecf946e40f96ab8923c82640045
blzzua <blzzua@users.noreply.github.com>
2022-10-07 19:30:46 +0000

    Delete alan_partridge_iii__london.py

--- 7-kyu/alan_partridge_iii__london.py
+++
@@ -1,8 +0,0 @@
-# https://www.codewars.com/kata/580a41b6d6df740d6100030c
-
-def alan(arr):
-    if {'Rejection', 'Disappointment', 'Backstabbing Central', 'Shattered Dreams Parkway'}.issubset(arr):
-        return 'Smell my cheese you mother!'
-    else:
-        return 'No, seriously, run. You will miss it.'
-
