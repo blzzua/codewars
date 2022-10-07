ebcdf60a2a2ab99ef20da683205e1107ea3e8812
blzzua <blzzua@github.com>
2022-10-07 11:25:02 +0300

    https://www.codewars.com/kata/5993fb6c4f5d9f770c0000f2

---
+++ 7-kyu/sum_a_list_but_ignore_any_duplicates.py
@@ -0,0 +1,7 @@
+# https://www.codewars.com/kata/5993fb6c4f5d9f770c0000f2
+
+from collections import Counter 
+def sum_no_duplicates(l):
+    c = Counter(l)
+    return sum(k for k in c if c[k]==1)
+
