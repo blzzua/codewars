6e4cc14b9089fe900725320a4513af71dc6e8782
blzzua <blzzua@github.com>
2022-10-07 22:17:10 +0300

    https://www.codewars.com/kata/5513795bd3fafb56c200049e

---
+++ 8-kyu/count_by_x.py
@@ -0,0 +1,8 @@
+# https://www.codewars.com/kata/5513795bd3fafb56c200049e
+
+def count_by(x, n):
+    """
+    Return a sequence of numbers counting by `x` `n` times.
+    """
+    return [x*i for i in range(1, n+1)]
+
