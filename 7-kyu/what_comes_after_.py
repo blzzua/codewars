4cdea9aed9f3b68630793676f80b6d959289e87f
blzzua <blzzua@users.noreply.github.com>
2022-10-07 19:31:35 +0000

    Delete what_comes_after_.py

--- 7-kyu/what_comes_after_.py
+++
@@ -1,5 +0,0 @@
-# https://www.codewars.com/kata/590f5b4a7bbb3e246000007d
-
-def comes_after(st, l): 
-    return ''.join([l2 for l1, l2 in zip(st,st[1:]) if l1.lower() == l.lower() and l2.isalpha()])
-
