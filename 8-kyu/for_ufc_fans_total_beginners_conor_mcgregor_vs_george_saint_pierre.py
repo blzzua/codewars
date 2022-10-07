5ec6c772ed174b52dde881b673fc68be30ba414c
blzzua <blzzua@github.com>
2022-10-07 22:17:11 +0300

    https://www.codewars.com/kata/582dafb611d576b745000b74

---
+++ 8-kyu/for_ufc_fans_total_beginners_conor_mcgregor_vs_george_saint_pierre.py
@@ -0,0 +1,5 @@
+# https://www.codewars.com/kata/582dafb611d576b745000b74
+
+def quote(fighter):
+    return {'george saint pierre': "I am not impressed by your performance.", 'conor mcgregor': "I'd like to take this chance to apologize.. To absolutely NOBODY!"}[fighter.lower()]
+
