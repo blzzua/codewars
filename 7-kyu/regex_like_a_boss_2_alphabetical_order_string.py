d77d9e6b306369e2d6e83857abe79db90077805c
blzzua <blzzua@github.com>
2022-10-07 11:25:01 +0300

    https://www.codewars.com/kata/5c1a334516537ccd450000d8

---
+++ 7-kyu/regex_like_a_boss_2_alphabetical_order_string.py
@@ -0,0 +1,4 @@
+# https://www.codewars.com/kata/5c1a334516537ccd450000d8
+
+REGEX = r'^ *a* *b* *c* *d* *e* *f* *g* *h* *i* *j* *k* *l* *m* *n* *o* *p* *q* *r* *s* *t* *u* *v* *w* *x* *y* *z* *\Z'
+
