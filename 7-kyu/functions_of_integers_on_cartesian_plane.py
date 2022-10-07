ecaeca0f632a2e41064ebd506f992a7e446266b0
blzzua <blzzua@github.com>
2022-10-07 22:17:14 +0300

    https://www.codewars.com/kata/559e3224324a2b6e66000046

---
+++ 7-kyu/functions_of_integers_on_cartesian_plane.py
@@ -0,0 +1,12 @@
+# https://www.codewars.com/kata/559e3224324a2b6e66000046
+
+def sumin(n):
+    return n*(n+1)*(2*n+1)/6
+    
+def sumax(n):
+    return n*(n+1)*(4*n-1)/6
+    
+def sumsum(n):
+    return n**2*(n+1)
+
+
