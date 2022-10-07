c3a653821372c41febe78c31b2edc936812eba9b
blzzua <blzzua@github.com>
2022-10-07 22:17:10 +0300

    https://www.codewars.com/kata/55a144eff5124e546400005a

---
+++ 8-kyu/classy_classes.py
@@ -0,0 +1,8 @@
+# https://www.codewars.com/kata/55a144eff5124e546400005a
+
+class Person:
+    def __init__(self,name,age):
+        self.name = name
+        self.name = age
+        self.info = f"{name}s age is {age}"
+
