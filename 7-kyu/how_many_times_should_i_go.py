a3ed462fbac0aa384fb6608d90fbf46f7364993e
blzzua <blzzua@github.com>
2022-10-07 22:17:12 +0300

    https://www.codewars.com/kata/57efcb78e77282f4790003d8

---
+++ 7-kyu/how_many_times_should_i_go.py
@@ -0,0 +1,6 @@
+# https://www.codewars.com/kata/57efcb78e77282f4790003d8
+
+from math import ceil
+def how_many_times(annual_price, individual_price):
+    return ceil(annual_price / individual_price)
+
