fb3451dcc5bac6a0594a2582670eabc4eca44f8f
blzzua <blzzua@github.com>
2022-10-07 11:24:58 +0300

    https://www.codewars.com/kata/561046a9f629a8aac000001d

---
+++ 7-kyu/array_comparator.py
@@ -0,0 +1,8 @@
+# https://www.codewars.com/kata/561046a9f629a8aac000001d
+
+def match_arrays(v, r):
+    return sum(1 for i in v if i in r)
+
+# DON'T remove
+verbose = False # set to True to diplay arrays being tested in the random tests
+
