fd81832afa0cfdac978dbf3210de2b6d1e2da5cb
blzzua <blzzua@github.com>
2022-10-07 22:17:12 +0300

    https://www.codewars.com/kata/5844a422cbd2279a0c000281

---
+++ 7-kyu/debug_the_functions_easy.py
@@ -0,0 +1,12 @@
+# https://www.codewars.com/kata/5844a422cbd2279a0c000281
+
+from math import prod
+def multi(l_st):
+    return prod(l_st )
+
+def add(l_st):
+    return sum(l_st)
+
+def reverse(string):
+    return string[::-1]
+
