45a4f54db13caf736b80c10fabb872238519f8c8
blzzua <blzzua@users.noreply.github.com>
2022-10-30 09:11:13 +0000

    Create sprint.sql

---
+++ sprint10/sprint.sql
@@ -0,0 +1,14 @@
+select c.name, c.city, sum(o.amount) as totalSum
+from customers c
+join orders o on o.customer_id = c.id 
+where o.amount between 100 and 3500 
+group by c.name, c.city
+order by c.city
+
+---------------------------------
+insert into customers (id, name, city, grade, salesperson_id)
+values(3006, 'Stefan Huk', 'Kyiv', 500, 5007);
+
+select * from customers
+where city in ('London', 'Kyiv')
+order by id;
