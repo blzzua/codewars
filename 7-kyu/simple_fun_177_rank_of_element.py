4446a3abf1f502c4177502a90ba9242704b7c6ae
blzzua <blzzua@users.noreply.github.com>
2023-08-21 18:21:18 +0300

    https://www.codewars.com/kata/58b8cc7e8e7121740700002d

---
+++ 7-kyu/simple_fun_177_rank_of_element.py
@@ -0,0 +1,14 @@
+# https://www.codewars.com/kata/58b8cc7e8e7121740700002d
+
+def rank_of_element(arr, i):
+    c1 = c2 = 0
+    val = arr[i]
+    for e in arr[:i]:
+        if e <= val:
+            c1 += 1
+    for e in arr[i+1:]:
+        if e < val:
+            c2 += 1
+    return c1 + c2
+
+# compact-clever sum(x <= arr[i] if n < i else x < arr[i] for n,x in enumerate(arr))
