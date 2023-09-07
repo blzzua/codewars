2f74f9dc036d634464d801146df3d8ac2975ca58
blzzua <blzzua@users.noreply.github.com>
2023-09-07 14:38:49 +0300

    https://www.codewars.com/kata/57c1ab3949324c321600013f

---
+++ 7-kyu/toleetspeak.py
@@ -0,0 +1,5 @@
+# https://www.codewars.com/kata/57c1ab3949324c321600013f
+
+LEET_DICT = { 'A' : '@', 'B' : '8', 'C' : '(', 'D' : 'D', 'E' : '3', 'F' : 'F', 'G' : '6', 'H' : '#', 'I' : '!', 'J' : 'J', 'K' : 'K', 'L' : '1', 'M' : 'M', 'N' : 'N', 'O' : '0', 'P' : 'P', 'Q' : 'Q', 'R' : 'R', 'S' : '$', 'T' : '7', 'U' : 'U', 'V' : 'V', 'W' : 'W', 'X' : 'X', 'Y' : 'Y', 'Z' : '2' }
+def to_leet_speak(s):
+    return s.translate(str.maketrans(LEET_DICT))
