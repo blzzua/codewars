f3301aaa58c104a56627c811cb2116c4d266a825
blzzua <blzzua@users.noreply.github.com>
2023-09-30 15:21:23 +0300

    Create two_sum.py

---
+++ 6-kyu/two_sum.py
@@ -0,0 +1,6 @@
+# two_sum.py
+def two_sum(numbers, target):
+    for i, a in enumerate(numbers):
+        for j, b in enumerate(numbers[i+1:], i+1):
+            if a + b == target:
+                return (i, j)
