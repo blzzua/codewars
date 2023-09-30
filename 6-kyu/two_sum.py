f96de076456dd9d32c0cfa9fac4c96b261f57ec8
blzzua <blzzua@users.noreply.github.com>
2023-09-30 15:31:32 +0300

    https://www.codewars.com/kata/52c31f8e6605bcc646000082

--- 6-kyu/two_sum.py
+++ 6-kyu/two_sum.py
@@ -1,4 +1,5 @@
-# two_sum.py
+# https://www.codewars.com/kata/52c31f8e6605bcc646000082
+
 def two_sum(numbers, target):
     for i, a in enumerate(numbers):
         for j, b in enumerate(numbers[i+1:], i+1):
