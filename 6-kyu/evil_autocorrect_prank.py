b90bd4e867a49ab1f3a160632c165b79de93ed53
blzzua <blzzua@users.noreply.github.com>
2023-10-03 12:22:30 +0300

    https://www.codewars.com/kata/538ae2eb7a4ba8c99b000439

---
+++ 6-kyu/evil_autocorrect_prank.py
@@ -0,0 +1,5 @@
+# https://www.codewars.com/kata/538ae2eb7a4ba8c99b000439
+
+import re
+def autocorrect(input):
+    return re.sub(r'\b(you+|u)\b', 'your sister', input, 0, re.MULTILINE | re.IGNORECASE)
