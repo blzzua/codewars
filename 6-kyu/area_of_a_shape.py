205e674c5e1572dc4cb02ba57e43e4934ea59437
blzzua <blzzua@users.noreply.github.com>
2023-10-03 15:22:40 +0300

    https://www.codewars.com/kata/5474be18b2bc28ff92000fbd

--- 6-kyu/area_of_a_shape.py
+++ 6-kyu/area_of_a_shape.py
@@ -7,4 +7,11 @@ def area_of_the_shape(f):
     X, Y = np.mgrid[-1:1+step:step, 0:1+step:step]
     res = sum([int(f(x,y)) for x,y in np.vstack((X.flatten(), Y.flatten())).T])/(cnt+1)/(cnt+1)
     return res
-  
+
+# best practics via np:
+def area_of_the_shape(f):
+    cnt = 500
+    xy, step = np.linspace(0., 1., 100, endpoint=False, retstep=True)
+    X, Y = np.meshgrid(xy + step/2, xy + step/2)
+    values = np.vectorize(f)(X, Y)
+    return np.mean(values)
