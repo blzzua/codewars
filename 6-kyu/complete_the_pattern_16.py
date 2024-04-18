e8cb840bbc7a22f4394976446ec5db2c01469fc1
blzzua <blzzua@users.noreply.github.com>
2024-04-18 21:36:04 +0300

    Create complete_the_pattern_16.py

---
+++ 6-kyu/complete_the_pattern_16.py
@@ -0,0 +1,10 @@
+# https://www.codewars.com/kata/55ae997d1c40a199e6000018
+
+def pattern(n):
+    s = [str(( n % 10 ))] * n
+    res = []
+    for i in range(n):
+        for j in range(i,n):
+            s[j] = str((n - i) % 10)
+        res.append(''.join(s))
+    return '\n'.join(res)
