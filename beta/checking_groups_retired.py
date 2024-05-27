10311d6b37c777394b9bd39c8036efb89b33af45
blzzua <blzzua@users.noreply.github.com>
2024-05-27 18:29:26 +0300

    https://www.codewars.com/kata/54b80308488cb6cd31000161

---
+++ beta/checking_groups_retired.py
@@ -0,0 +1,28 @@
+# https://www.codewars.com/kata/54b80308488cb6cd31000161
+
+def group_check(s):
+    wait = []
+    l = list(s)
+    while l:
+        char = l.pop(0)
+        if char == '(':
+            wait.append(')')
+            continue
+        elif char == '{':
+            wait.append('}')
+            continue
+        elif char == '[':
+            wait.append(']')
+            continue
+        elif wait and char == wait[-1]:
+            wait.pop()
+            continue
+        if wait and l != wait[-1] and l in wait:
+            return False
+        if len(wait) == 0 and char in ')}]':
+            return False
+    if wait:
+        return False
+    else:
+        return True
+
