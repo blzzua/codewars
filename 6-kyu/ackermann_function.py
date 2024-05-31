d67a7fc88c4da346aa25ef96c2bd7c504a3dff12
blzzua <blzzua@users.noreply.github.com>
2024-05-31 14:38:24 +0300

    https://www.codewars.com/kata/53ad69892a27079b34000bd9

--- 6-kyu/ackermann_function.py
+++ 6-kyu/ackermann_function.py
@@ -10,3 +10,20 @@ def Ackermann(m, n):
         return Ackermann(m - 1, 1)
     return Ackermann(m - 1, Ackermann(m, n - 1))
 
+
+# limited m <= 4 solution: https://en.wikipedia.org/wiki/Ackermann_function
+def Ackermann(m, n):
+    if m == 0: 
+        return n + 1
+    if m == 1:
+        return n + 2
+    if m == 2:
+        return 2 * n + 3 
+    if m == 3:
+        return 2 ** (n + 3) - 3
+    if m == 4:
+        res = 1
+        while n + 3 > 0:
+            res = 2 ** res
+            n -= 1
+        return res - 3
