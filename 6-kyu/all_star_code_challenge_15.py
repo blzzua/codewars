ef1da01b9adb374f5b1c0217784c6632f1636570
blzzua <blzzua@users.noreply.github.com>
2024-06-10 13:30:05 +0300

    https://www.codewars.com/kata/586560a639c5ab3a260000f3

--- 6-kyu/all_star_code_challenge_15.py
+++ 6-kyu/all_star_code_challenge_15.py
@@ -4,3 +4,10 @@ def rotate(str_):
     l = len(str_)
     s = str_ + str_
     return [ (s[i:]+s[:i])[:l] for i in range(1, l+1)]
+
+
+# this kata can be solved with collections.deque
+import collections
+def rotate(str_):
+    queue = collections.deque(str_)
+    return [queue.rotate(-1) or ''.join(queue) for _ in str_]
