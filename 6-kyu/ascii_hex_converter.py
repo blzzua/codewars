6e3eadd62b07b3c0f81dd543763b53d9690869be
blzzua <blzzua@users.noreply.github.com>
2025-05-01 14:15:35 +0300

    https://www.codewars.com/kata/52fea6fd158f0576b8000089

---
+++ 6-kyu/ascii_hex_converter.py
@@ -0,0 +1,12 @@
+# https://www.codewars.com/kata/52fea6fd158f0576b8000089
+
+class Converter():
+    @staticmethod
+    def to_ascii(h):
+        #return ''.join(chr(int(a+b,16)) for a,b in itertools.batched(h,2)) # itertools.batched implemented only from python 3.12 
+        return ''.join(chr(int(a+b,16)) for a, b in zip(h[::2], h[1::2]))
+        
+    
+    @staticmethod
+    def to_hex(s):
+        return ''.join(map(lambda c: '{:x}'.format(ord(c)), s))
