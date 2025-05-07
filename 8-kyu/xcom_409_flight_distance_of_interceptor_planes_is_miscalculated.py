e8d082d14a2011471d3c645acbc70e36a0aea101
blzzua <blzzua@users.noreply.github.com>
2025-05-07 09:56:09 +0300

    https://www.codewars.com/kata/67b7a527c9f842fd3b02adb8

---
+++ 8-kyu/xcom_409_flight_distance_of_interceptor_planes_is_miscalculated.py
@@ -0,0 +1,13 @@
+# https://www.codewars.com/kata/67b7a527c9f842fd3b02adb8
+
+# speed of aircrafts is given in *knots*
+# travelTime is in *minutes*
+# travel distance should be returned in *kilometers*
+
+def travel_distance(avg_speed, travel_time):
+    KM_PER_MILE = 1.852
+    travel_hours = travel_time / 60
+    travel_miles = avg_speed * travel_hours
+    travel_kms = travel_miles * KM_PER_MILE
+    return travel_kms
+
