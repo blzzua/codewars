efcb712b9fd1745bfe57875f3d6ddd2924d46253
blzzua <blzzua@users.noreply.github.com>
2025-06-14 18:08:18 +0300

    Update simple_fun_212_processing_requests.py

--- 6-kyu/simple_fun_212_processing_requests.py
+++ 6-kyu/simple_fun_212_processing_requests.py
@@ -11,3 +11,18 @@ def processing_requests(servers, requests):
         if ind >= 0 and requests[ind] <= 2*s:
             res.append(requests.pop(ind))
     return len(res)
+
+# improved 
+import heapq
+def processing_requests(servers, requests):
+    rs = requests[:]
+    heapq.heapify(rs)
+    res = []
+    ind = len(rs)
+    for s in range(servers, 0, -1):
+        while rs:
+            if request := rs.pop() <= 2*s:
+                res.append(request)
+                break
+            # else: just drop last request. sorry request, this world is too cruel
+    return len(res)
