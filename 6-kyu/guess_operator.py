bbdd6e97bdb0ea5b8b3e7d90c80dc9f78f7f00bb
blzzua <blzzua@users.noreply.github.com>
2025-06-27 10:31:35 +0300

    https://www.codewars.com/kata/60512be8bbc51a000a83d767

---
+++ 6-kyu/guess_operator.py
@@ -0,0 +1,15 @@
+# https://www.codewars.com/kata/60512be8bbc51a000a83d767
+
+def operator(a, n, b):
+    if n == 0:
+        return b + 1
+    if n == 1:
+        return a + b
+    if n == 2:
+        return a * b
+    if n == 3:
+        return a ** b
+    if n == 4:
+        if b == 0:
+            return 1
+        return a **  operator(a, n, b - 1)
