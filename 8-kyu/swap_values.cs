14f5c489f3fab273f48add3f3f50e5a629eb1ab5
blzzua <blzzua@github.com>
2026-09-25 21:04:07 +0300

    https://www.codewars.com/kata/5388f0e00b24c5635e000fc6

---
+++ 8-kyu/swap_values.cs
@@ -0,0 +1,18 @@
+// https://www.codewars.com/kata/5388f0e00b24c5635e000fc6
+
+public class Swapper
+{
+    public object[] Arguments { get; private set; }
+    
+    public Swapper(object[] args)
+    {
+        Arguments = args;
+    }
+    
+    public void SwapValues()
+    {
+        object temp = Arguments[0];
+        Arguments[0] = Arguments[1];
+        Arguments[1] = temp;
+    }
+}
